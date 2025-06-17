621fd8b88c30568cf4462095645f1c178549d4ab
Israel Gabice <igabice@gmail.com>
2025-06-17 21:34:54 +0300

    chore: setup backend structure - added docker-compose for mysql-db & redis - updated backend readme with instructions - setup swagger doc - errorHandler middleware - request body validation middleware - base class for api errors

---
+++ backend/src/docs/swagger.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { name } from "../../package.json";
+
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: `${name} API Documentation`,
+      version: "1.0.0",
+      description: "API documentation for Adcash Campaign Management",
+    },
+    license: {
+      name: "MIT",
+    },
+    servers: [
+      {
+        url: "https://adcash.onrender.com/v1",
+        description: "Live server",
+      },
+      {
+        url: "http://localhost:3000/v1",
+        description: "Development server",
+      },
+    ],
+    externalDocs: {
+      description: "swagger.json",
+      url: "/v1/docs/swagger.json",
+    },
+  },
+  apis: ["src/docs/*.yml", "src/routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const router = express.Router();
+
+router.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+router.get("/swagger.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+
+export default router;
